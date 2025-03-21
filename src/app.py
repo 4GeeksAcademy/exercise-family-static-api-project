577f90697122ec91a2ed670201605bb221c8e3be
kazou92 <99810202+kazou92@users.noreply.github.com>
2025-03-21 12:58:30 +0000

    family static api loaded

--- src/app.py
+++ src/app.py
@@ -27,18 +27,47 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body =members
     return jsonify(response_body), 200
+   
+@app.route('/members', methods=['POST'])
+def add_members():
+    body = request.json
+    member={
+        "first_name":body["first_name"],
+        "age":body["age"], 
+        "lucky_numbers":body["lucky_numbers"]
+    }
+      
+    jackson_family.add_member(member)
+    return jsonify(member), 200
+
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_members(member_id):
+    sucess = jackson_family.delete_member(member_id)
+    if sucess:
+        return jsonify({"msg":"member delete succesfully"}),200
+    return jsonify({"error":"member no se ha encontrado "}),400
+
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    sucess = jackson_family.get_member(member_id)
+    if sucess:
+        return jsonify({"SUCCESS":sucess}),200
+    return jsonify({"error":"member no se ha encontrado"}),404
+   
+  
+    
+ 
+
+
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+           
